30a77ead9cf5eef31980092c365530e6def007cc
frankwuyue <wuyuefrank@gmail.com>
2018-04-07 17:43:29 +0900

    change card body column type to text and change card body form to text_area

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180325074000) do
+ActiveRecord::Schema.define(version: 20180407082218) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,7 +27,7 @@ ActiveRecord::Schema.define(version: 20180325074000) do
 
   create_table "cards", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "title"
-    t.string "body"
+    t.text "body"
     t.uuid "board_id"
     t.uuid "user_id"
     t.datetime "created_at", null: false
