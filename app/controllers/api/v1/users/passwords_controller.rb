f4b5e067ad745b7a7b717f6d6566eaa3d72b2f54
naohiro-t <naohiro@xenon.io>
2018-04-09 00:33:04 +0900

    override devise::passwords_controller to return json

---
+++ app/controllers/api/v1/users/passwords_controller.rb
@@ -0,0 +1,27 @@
+class Api::V1::Users::PasswordsController < Devise::SessionsController
+  respond_to :json
+  # POST /api/v1/password
+  # send reset password
+  def create
+    self.resource = resource_class.send_reset_password_instructions(resource_params)
+    if successfully_sent?(resource)
+      head :ok
+    else
+      # TODO: we might want to return error message instead of just returning resource...?
+      render json: resource
+    end
+  end
+
+  # PUT /api/v1/password
+  def update
+    self.resource = resource_class.reset_password_by_token(resource_params)
+    if resource.errors.empty?
+      resource.unlock_access! if unlockable?(resource)
+      render json: {user_id: resource.id, token: JWTWrapper.encode({ user_id: resource.id }), expires_in: Rails.application.secrets.jwt_expiration_hours}
+    else
+      set_minimum_password_length
+      respond_with resource
+    end
+  end
+
+end
