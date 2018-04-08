dc1a07ea0556a22bf4b8d581cffde913c6c5edbb
naohiro-t <naohiro@xenon.io>
2018-04-08 23:15:54 +0900

    override devise::registrations_controller to return json

---
+++ app/controllers/api/v1/users/registrations_controller.rb
@@ -0,0 +1,15 @@
+class Api::V1::Users::RegistrationsController < Devise::RegistrationsController
+
+  # Override create to return json
+  def create
+    build_resource(sign_up_params)
+    resource.save
+    if resource.persisted?
+      render json: {user_id: resource.id, token: JWTWrapper.encode({ user_id: resource.id }), expires_in: Rails.application.secrets.jwt_expiration_hours}
+    else
+      clean_up_passwords resource
+      set_minimum_password_length
+      respond_with resource
+    end
+  end
+end
