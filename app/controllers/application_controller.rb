bd701b2d1737be40edceca217a1102accf7f916b
naohiro-t <naohiro@xenon.io>
2018-04-08 20:33:20 +0900

    better explanation for configure_permitted_parameters in application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,12 +3,11 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
-
+  # accept more attributes for devise authentication.
+  # I added "username"(others are default) to default devise attributes here.
   def configure_permitted_parameters
     added_attrs = [:username, :email, :password, :password_confirmation, :remember_me]
-    # signin_attrs = added_attrs.push(:login)[:login, :username, :email, :password, :password_confirmation, :remember_me]
     devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
-    # devise_parameter_sanitizer.permit :sign_in, keys: signin_attrs
     devise_parameter_sanitizer.permit :account_update, keys: added_attrs
   end
 end
