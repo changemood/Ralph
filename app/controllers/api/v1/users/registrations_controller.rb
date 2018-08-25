c44a6d7e7779e7e16a569e6d2adc6f76b5d9d7b6
naohiro-t <takahashi.naohiro1222@gmail.com>
2018-08-25 14:23:21 +0900

    update response for registration#update

--- app/controllers/api/v1/users/registrations_controller.rb
+++ app/controllers/api/v1/users/registrations_controller.rb
@@ -15,6 +15,22 @@ class Api::V1::Users::RegistrationsController < Devise::RegistrationsController
     end
   end
 
+  # PUT /resource
+  # We need to use a copy of the resource because we don't want to change
+  # the current user in place.
+  def update
+    self.resource = resource_class.to_adapter.get!(send(:"current_#{resource_name}").to_key)
+    prev_unconfirmed_email = resource.unconfirmed_email if resource.respond_to?(:unconfirmed_email)
+
+    resource_updated = update_resource(resource, account_update_params)
+    yield resource if block_given?
+    if resource_updated
+      render json: resource, status: :ok      
+    else
+      respond_with resource
+    end
+  end
+
   protected
   # NOTE: Overwriting this because we don't require password to update user
   # By default on devise, require a password checks on update.
