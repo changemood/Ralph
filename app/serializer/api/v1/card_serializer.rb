5c11fba48fb7efb8fe2772feb3176bfd453814f9
naohiro-t <takahashi.naohiro1222@gmail.com>
2018-08-26 12:46:46 +0900

    add board_id to card serializer

--- app/serializer/api/v1/card_serializer.rb
+++ app/serializer/api/v1/card_serializer.rb
@@ -1,7 +1,7 @@
 module Api
   module V1
     class CardSerializer < ActiveModel::Serializer
-      attributes :id, :title, :body, :created_at, :updated_at, :ancestry, :latest_sr_event
+      attributes :id, :title, :body, :created_at, :updated_at, :board_id, :ancestry, :latest_sr_event
     end
   end
 end
